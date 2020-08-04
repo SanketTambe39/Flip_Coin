36bf98e7d3518bcd8644939471da472891b203e0
SanketTambe39 <sankettambe39@gmail.com>
2020-08-04 17:40:49 +0530

    Flip coin until someone get 21

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,16 +3,28 @@
 heads=0
 tails=0
 
-for ((i=0;i<100;i++))
+#for((i=0;i<100;i++))
+while ((1))
 do
 	flip=$((RANDOM%2))
-	if [ $flip -eq 0 ]
+
+        if [ $flip -eq 0 ]
+        then
+                heads=$(($heads+1))
+        else
+                tails=$(($tails+1))
+        fi
+
+	if [[ $heads -eq 21 || $tails -eq 21 ]]
 	then
-		heads=$(($heads+1))
-	else
-		tails=$(($tails+1))
+		break
 	fi
+
 done
 
-echo " head won "$heads" times "
-echo " Tail won "$tails" times "
+if [ $heads -eq 21 ]
+then
+	echo " head won "$heads" times "
+else
+	echo " Tail won "$tails" times "
+fi
